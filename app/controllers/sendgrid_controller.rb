d80ccabb7a229315869a6eeb8bdfabb9811e68d3
sachin choudhary <sachin@headerlabs.com>
2018-06-06 17:33:08 +0530

    escaping the nil exception

--- app/controllers/sendgrid_controller.rb
+++ app/controllers/sendgrid_controller.rb
@@ -3,7 +3,7 @@ class SendgridController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:webhooks]
   def webhooks
 
-    smtp_id = params['_json'].first['smtp-id'].scan(/<(.*)>/).last.first
+    smtp_id = params['_json'].first['smtp-id'].scan(/<(.*)>/).last.first rescue nil
     logger.info "smtp id #{smtp_id}---"
     if smtp_id && notification = Notification.where(send_grid_identifier: smtp_id).take
       params['_json'].each do|payload|
